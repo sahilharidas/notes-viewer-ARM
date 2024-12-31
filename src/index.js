d36b555918aebf19a02635ed8c0386bd1f632361
Sahil Haridas <sahilharidas@gmail.com>
2024-12-31 13:53:50 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import NotesViewer from './NotesViewer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NotesViewer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
