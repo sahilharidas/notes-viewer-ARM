ef2889afd9f7942ac7fe6f82e01ddc9a84d47397
Sahil Haridas <sahilharidas@gmail.com>
2024-12-31 17:04:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+// src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import NotesViewer from './NotesViewer';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <NotesViewer />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <React.StrictMode>
+   <NotesViewer />
+ </React.StrictMode>,
+ document.getElementById('root')
 );
+
+// Register the service worker
